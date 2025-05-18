47a78b7d88fcddee910ca2c7d5a2937bc94e10d9
Anup Agarwal <anupagarwal@Anups-MacBook-Pro.local>
2025-05-18 20:28:54 +0530

    added sqlite db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,48 +1,23 @@
 import Link from 'next/link';
 import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
-export default function Home() {
+export default async function Meals() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
-        <div>
-          <div className={classes.hero}>
-            <h1>NextLevel Food for NextLevel Foodies</h1>
-            <p>Taste & share food from all over the world.</p>
-          </div>
-          <div className={classes.cta}>
-            <Link href="/community">Join the Community</Link>
-            <Link href="/meals">Explore Meals</Link>
-          </div>
-        </div>
+        <h1>Delicious meals, created <span>by you</span></h1>
+        <p>Choose your favourite recipe and cook it yourself. It is easy and fun</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">
+            Share your favourite recipe</Link>
+        </p>
       </header>
-      <main>
-        <section className={classes.section}>
-          <h2>How it works</h2>
-          <p>
-            NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
-            connect with other food lovers.
-          </p>
-          <p>
-            NextLevel Food is a place to discover new dishes, and to connect
-            with other food lovers.
-          </p>
-        </section>
-
-        <section className={classes.section}>
-          <h2>Why NextLevel Food?</h2>
-          <p>
-            NextLevel Food is a platform for foodies to share their favorite
-            recipes with the world. It&apos;s a place to discover new dishes, and to
-            connect with other food lovers.
-          </p>
-          <p>
-            NextLevel Food is a place to discover new dishes, and to connect
-            with other food lovers.
-          </p>
-        </section>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
